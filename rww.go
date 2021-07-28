830432a7b2c8d69f012489a2332a69cd3caf30e1
Shayan Hosseini <shayan.hosseiny@gmail.com>
2021-07-28 12:05:14 -0700

    Remove unnecessary methods from rww

--- rww.go
+++ rww.go
@@ -11,14 +11,6 @@ func (rww *ResponseWriterWrapper) Status() int {
 	return rww.status
 }
 
-func (rww *ResponseWriterWrapper) Header() http.Header {
-	return rww.ResponseWriter.Header()
-}
-
-func (rww *ResponseWriterWrapper) Write(data []byte) (int, error) {
-	return rww.ResponseWriter.Write(data)
-}
-
 func (rww *ResponseWriterWrapper) WriteHeader(statusCode int) {
 	rww.status = statusCode
 	rww.ResponseWriter.WriteHeader(statusCode)
